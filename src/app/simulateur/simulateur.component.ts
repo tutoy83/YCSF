9c30d105c5c87c0ffb78fb6ae47224b3c61f9e47
root <root@vps12672.brs.infra.privateheberg.net>
2023-04-06 22:16:51 +0200

    Implementation 3 stages

--- src/app/simulateur/simulateur.component.ts
+++ src/app/simulateur/simulateur.component.ts
@@ -27,9 +27,8 @@ export class SimulateurComponent {
     { nom: 'Catamaran Ados', prix: 146},
     { nom: 'Catamaran Adultes', prix: 174},
     { nom: 'Planche Adultes', prix: 145},
-    { nom: 'Multi', prix: 130},
-    { nom: 'Cotisation Club', prix: 19},
-    { nom: 'Passeport Voile', prix: 12},
+    { nom: 'Dériveur (Vago/laser)', prix: 140},
+    { nom: 'Multi-Activités', prix: 130},
   ];
 
 
@@ -42,29 +41,34 @@ export class SimulateurComponent {
 
 
   ajouterStageDemande(stage: Stage): void {
-    const stageDemande: StageDemande = {
-      nom: stage.nom,
-      prixPublic: stage.prix,
-      prixRemise: stage.prix
-    };
+    const nbStages = this.stagesDemandesArr.length;
 
-    this.stagesDemandesArr.push(stageDemande);
-    for (let i = 0; i < this.stagesDemandesArr.length; i++) {
-      for (let j = i + 1; j < this.stagesDemandesArr.length; j++) {
-        if (this.stagesDemandesArr[i].prixPublic === this.stagesDemandesArr[j].prixPublic) {
-          this.stagesDemandesArr[j].prixPublic += 0.000001 * (j - i);
+    if (nbStages < 3){
+      const stageDemande: StageDemande = {
+        nom: stage.nom,
+        prixPublic: stage.prix,
+        prixRemise: stage.prix
+      };
+
+      this.stagesDemandesArr.push(stageDemande);
+      for (let i = 0; i < this.stagesDemandesArr.length; i++) {
+        for (let j = i + 1; j < this.stagesDemandesArr.length; j++) {
+          if (this.stagesDemandesArr[i].prixPublic === this.stagesDemandesArr[j].prixPublic) {
+            this.stagesDemandesArr[j].prixPublic += 0.000001 * (j - i);
+          }
         }
       }
+      this.totalCalc();
+    }else{
+      alert("⚠️ Vous avez déjà 3 stages ");
     }
-    this.totalCalc();
   }
 
   totalCalc(): number {
-    let total = 0;
     const nbStages = this.stagesDemandesArr.length;
   
     if (nbStages === 0) {
-      return total;
+      return this.total;
     } else if (nbStages === 1) {
       return this.stagesDemandesArr[0].prixRemise;
     } else if (nbStages === 2) {
@@ -75,25 +79,78 @@ export class SimulateurComponent {
       if (moussaillonsOuKayak) {
         if (stage1.nom !== 'Moussaillons' && stage1.nom !== 'Kayak') {
             stage1.prixRemise = stage1.prixPublic * 0.95;
-            total += stage1.prixRemise;
+            this.total += stage1.prixRemise;
         } else if (stage2.nom !== 'Moussaillons' && stage2.nom !== 'Kayak') {
             stage2.prixRemise = stage2.prixPublic * 0.95;
-            total += stage2.prixRemise;
+            this.total += stage2.prixRemise;
         } else {
-            total += stage1.prixPublic + stage2.prixPublic;
+          this.total += stage1.prixPublic + stage2.prixPublic;
         }
     } else {
         const stageAvecPrixPlusGrand = stage1.prixPublic > stage2.prixPublic ? stage1 : stage2;
         const stageAvecPrixPlusPetit = stage1.prixPublic > stage2.prixPublic ? stage2 : stage1;
       
         stageAvecPrixPlusGrand.prixRemise = stageAvecPrixPlusGrand.prixPublic * 0.95;
-        total += stageAvecPrixPlusGrand.prixRemise;
-        total += stageAvecPrixPlusPetit.prixPublic;
+        this.total += stageAvecPrixPlusGrand.prixRemise;
+        this.total += stageAvecPrixPlusPetit.prixPublic;
     }
     
     }
+    else if (nbStages === 3) {
+      const stage1 = this.stagesDemandesArr[0];
+      const stage2 = this.stagesDemandesArr[1];
+      const stage3 = this.stagesDemandesArr[2];
+      const moussaillonsOuKayak = stage1.nom === 'Moussaillons' || stage1.nom === 'Kayak' || stage2.nom === 'Moussaillons' || stage2.nom === 'Kayak' || stage3.nom === 'Moussaillons' || stage3.nom === 'Kayak';
+    
+      if (moussaillonsOuKayak) {
+        if (stage1.nom === 'Moussaillons' && stage2.nom === 'Moussaillons' && stage3.nom === 'Moussaillons' || stage1.nom === 'Kayak' && stage2.nom === 'Kayak' && stage3.nom === 'Kayak') {
+            //INTERDIT - INTERDIT - INTERDIT
+            this.total += stage1.prixPublic + stage2.prixPublic + stage3.prixPublic;
+        } else if ((stage1.nom === 'Moussaillons' || stage1.nom === 'Kayak') && (stage2.nom === 'Moussaillons' || stage2.nom === 'Kayak') && stage3.nom !== 'Moussaillons' && stage3.nom !== 'Kayak') {
+            //INTERDIT - INTERDIT - NORMAL  
+            stage3.prixRemise = stage3.prixPublic * 0.9;
+            this.total += stage1.prixPublic + stage2.prixPublic + stage3.prixRemise;
+        } else if ((stage1.nom === 'Moussaillons' || stage1.nom === 'Kayak') && stage2.nom !== 'Moussaillons' && stage2.nom !== 'Kayak' && (stage3.nom === 'Moussaillons' || stage3.nom === 'Kayak')) {
+            //INTERDIT - NORMAL - INTERDIT
+            stage2.prixRemise = stage2.prixPublic * 0.9;
+            alert('y');
+            this.total += stage1.prixPublic + stage2.prixRemise + stage3.prixPublic;
+        }else if ((stage1.nom === 'Moussaillons' || stage1.nom === 'Kayak') && stage2.nom !== 'Moussaillons' && stage2.nom !== 'Kayak' && stage3.nom !== 'Moussaillons' && stage3.nom !== 'Kayak') {
+            //INTERDIT - NORMAL - NORMAL
+            alert("INTERDIT - NORMAL - NORMAL");
+            stage2.prixRemise = stage2.prixPublic * 0.9;
+            stage3.prixRemise = stage3.prixPublic * 0.95;
+            this.total += stage1.prixPublic + stage2.prixRemise + stage3.prixPublic;
+        } else if (stage1.nom !== 'Moussaillons' && stage1.nom !== 'Kayak' && (stage2.nom !== 'Moussaillons' && stage2.nom !== 'Kayak') && (stage3.nom === 'Moussaillons' || stage3.nom === 'Kayak')) {
+            //NORMAL - NORMAL - INTERDIT
+            stage1.prixRemise = stage1.prixPublic * 0.9;
+            stage2.prixRemise = stage2.prixPublic * 0.95;
+            this.total += stage1.prixRemise + stage2.prixRemise + stage3.prixPublic;
+        
+        } else if (stage1.nom !== 'Moussaillons' && stage1.nom !== 'Kayak' && (stage2.nom === 'Moussaillons' || stage2.nom === 'Kayak') && (stage3.nom === 'Moussaillons' || stage3.nom === 'Kayak')) {
+            //NORMAL - INTERDIT - INTERDIT
+            stage1.prixRemise = stage1.prixPublic * 0.9;
+            this.total += stage1.prixRemise + stage2.prixPublic + stage3.prixPublic;
+        }
+        } else if (stage1.nom !== 'Moussaillons' && stage1.nom !== 'Kayak' && (stage2.nom === 'Moussaillons' || stage2.nom === 'Kayak') && stage3.nom !== 'Moussaillons' && stage3.nom !== 'Kayak') {
+          //NORMAL - INTERDIT - NORMAL                                                       TO DOUBLE CHECK
+          stage1.prixRemise = stage1.prixPublic * 0.9;
+          stage3.prixRemise = stage3.prixPublic * 0.95;
+          this.total += stage1.prixRemise + stage2.prixPublic + stage3.prixRemise;
+      
+      } else {
+        alert("ttt - NORMAL - NORMAL");
+
+          const stagesSortedByPrice = this.stagesDemandesArr.sort((a, b) => b.prixPublic - a.prixPublic);
+          stagesSortedByPrice[0].prixRemise = stagesSortedByPrice[0].prixPublic * 0.9;
+          stagesSortedByPrice[1].prixRemise = stagesSortedByPrice[1].prixPublic * 0.95;
+          this.total += stagesSortedByPrice[0].prixRemise + stagesSortedByPrice[1].prixRemise + stagesSortedByPrice[2].prixPublic;
+      }
+  }
+
+
     console.log("Fin total", this.stagesDemandesArr);
-    return total;
+    return this.total;
   }
   
   
